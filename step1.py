587f8a77497d4aa12f58f9627dd067cee52bf793
Thomas <thomasaubrun63@gmail.com>
2023-12-06 16:25:22 +0100

    Update
    Update step 2

--- step1.py
+++ step1.py
@@ -1,26 +1,16 @@
-import os
 import pandas as pd
 
-# Définir le chemin vers les fichiers
-chemin = "./Desktop/IUT_SD3_Github/iut_sd3_accidents/data"
-
-# Construire les chemins complets vers les fichiers
-file1 = os.path.join(chemin, "carac.csv")
-file2 = os.path.join(chemin, "lieux.csv")
-file3 = os.path.join(chemin, "veh.csv")
-file4 = os.path.join(chemin, "vict.csv")
-
-# Lire les fichiers
-df1 = pd.read_csv(file1)
-df2 = pd.read_csv(file2)
-df3 = pd.read_csv(file3)
-df4 = pd.read_csv(file4)
+# Charger les fichiers CSV
+carac = pd.read_csv("data/carac.csv", sep=';')
+lieux = pd.read_csv("data/lieux.csv", sep=';')
+veh = pd.read_csv("data/veh.csv", sep=';')
+vict = pd.read_csv("data/vict.csv", sep=';')
 
 # Fusionner les DataFrames
-merged_df = pd.concat([df1, df2, df3, df4], ignore_index=True)
+victime = vict.merge(veh, on=['Num_Acc', 'num_veh'])
+accident = carac.merge(lieux, on='Num_Acc')
+result_df = victime.merge(accident, on='Num_Acc')
 
-# Afficher le DataFrame fusionné
-print(merged_df)
+result_df.to_csv("step1/merged_data.csv", index=False)
 
-# Écrire le DataFrame fusionné dans un nouveau fichier
-merged_df.to_csv(os.path.join(chemin, "merged_data.csv"), index=False)
+print("ok")
